f8ebb36cc05faf32eff6378b24b89ef42e5d66d9
thiago <vitinhoeaice@gmail.com>
2024-08-17 21:10:35 -0300

    Desafio trilha-basico-sintaxe-dio concluido
    Signed-off-by: thiago <vitinhoeaice@gmail.com>

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -8,6 +8,9 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataAberturaConta = LocalDate.now();
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataAberturaFormatada = dataAberturaConta.format(formatter1);
 
         System.out.print("Digite o seu nome: ");
         String nome = scanner.nextLine();
@@ -16,25 +19,29 @@ public class ContaTerminal {
         String dataNascimentoStr = scanner.nextLine();
         LocalDate dataNascimento = LocalDate.parse(dataNascimentoStr, formatter);
 
-        System.out.print("Digite o Número da Conta: ");
-        int numeroConta = scanner.nextInt();
+        if (dataNascimento.plusYears(18).isBefore(LocalDate.now()) || dataNascimento.plusYears(18).isEqual(LocalDate.now())) {
+            System.out.println("Certo, você é maior de idade e pode abrir a sua conta.");
 
-        System.out.print("Digite sua agência: ");
-        int agencia = scanner.nextInt();
+            System.out.print("Digite o Número da Conta: ");
+            int numeroConta = scanner.nextInt();
 
-        System.out.print("Digite o saldo inicial: ");
-        double saldo = scanner.nextDouble();
+            System.out.print("Digite sua agência: ");
+            int agencia = scanner.nextInt();
 
-        Cliente cliente = new Cliente(nome, dataNascimento);
+            System.out.print("Digite o saldo inicial: ");
+            double saldo = scanner.nextDouble();
 
-        ContaCadastro novaConta = new ContaCadastro(cliente, agencia, numeroConta, saldo, LocalDate.now());
+            Cliente cliente = new Cliente(nome, dataNascimento);
 
-        System.out.println(novaConta);
+            ContaCadastro novaConta = new ContaCadastro(cliente, agencia, numeroConta, saldo, LocalDate.now());
 
-        //TODO: Exibir as menssagens para o nosso usuário
+            System.out.println("Olá, " + nome + "! Obrigado por criar uma conta em nosso Banco.");
+            System.out.println("Sua agencia é: " + agencia + " Sua conta: " + numeroConta);
+            System.out.println("E o seu saldo é: " + saldo);
+            System.out.println("E a data de Abertura de sua conta é: " + dataAberturaFormatada);
 
-        //TODO: Obter pela scanner os valores digitados no terminal
-
-        //TODO: Exibir a menssagem conta criada
+        } else {
+            System.out.println("OPS! Você precisa ser +18 anos para abrir sua conta.");
+        }
     }
 }
